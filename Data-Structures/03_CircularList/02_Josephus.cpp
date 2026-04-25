d1cf7bc350f98289f43cea9f3be3e1f56791a2da
yoozt070518 <468388107@qq.com>
2026-04-25 11:21:43 +0800

    带虚拟头节点

--- Data-Structures/03_CircularList/02_Josephus.cpp
+++ Data-Structures/03_CircularList/02_Josephus.cpp
@@ -1,68 +1,128 @@
-// 约瑟夫环问题
-// 已知n个人围坐在一张圆桌周围，编号为k的人开始报数
-// 数到m的那个人出列，它的下一个人又开始从1报数，数到m的人又出列
-// 依次规律重复
-// 知道全部出列，输出人的出列顺序
-
-// 先重写一遍环形链表的类
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 struct Node{
-    int data;
-    Node* next;
-    Node(int val = 0){
-        data = val;
-        next = nullptr;
-    }
+        int data;
+        Node* next;
+        Node(int val = 0){
+            data = val;
+            next = nullptr;
+        }    
 };
 
-void Joseph(Node* head, int k ,int m){
-    if (head == nullptr) return;
-    Node* p = head;
-    Node* q = head;
-    while(q->next!=head){
-        q = q->next;
+class CircleLink{
+public:
+    CircleLink(){
+        head = new Node();
+        tail = head;
+        head->next = head;
     }
+    ~CircleLink(){
+        Node* curr = head->next;
+        while(curr!=head){
+            head->next = curr->next;
+            delete curr;
+            curr = head->next;
+        }
+        delete head;
+    }
+public:
+    // 尾插法
+    void InsertTail(int val){
+        Node* node = new Node(val);
+        node->next = tail->next;
+        tail->next = node;
+        tail = node;
+    }
+    // 头插法
+    void InsertHead(int val){
+        Node* node = new Node(val);
+        node->next = head->next;
+        head->next = node;
+        if(node->next==head){
+            tail = node;
+        }
+    }
+
+    // 删除节点
+    void  Remove(int val){
+        Node* q = head;
+        Node* p = head->next;
+        while(p != head){
+            if(p->data == val){
+                q->next = p->next;
+                delete p;
+                if(q->next == head){
+                    tail = q;
+                }
+                return;
+            }
+            p = p->next;
+            q = q->next;
+        }
+    }
+
+    // 查询
+    bool Find(int val) const{
+        Node* p = head->next;
+        while(p != head){
+            if(p->data == val){
+                return true;
+            }
+            p = p->next;
+        }
+        return false;
+    }
+    void Show() const{
+        Node* p  = head->next;
+        while(p != head){
+            cout << p->data << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+private:
+friend void Joseph(CircleLink& link, int k ,int m);
+    Node* head;
+    Node* tail; 
+};
+
+void Joseph(CircleLink& link, int k ,int m){
+    if(link.head->next == link.head){
+        return;
+    }
+    link.tail->next = link.head->next;
+    Node* p = link.head->next;
+    Node* q = link.tail;
     for(int i = 1;i<k;++i){
         q = p;
         p = p->next;
     }
-    while(1){
+    while(q!=p){
         for(int i = 1;i<m;++i){
             q = p;
             p = p->next;
         }
-        
         cout << p->data << " ";
-        if(p==q){
-            delete p;
-            break;
-        }
         q->next = p->next;
         delete p;
         p = q->next;
-
     }
+    cout << p->data << endl;
+    link.head->next = p;
+    p->next = link.head;
+    link.tail = p;
+    
     
 }
+
+
+
+
+
+
 int main(){
-    Node* head = new Node(1);
-    Node* n2 = new Node(2);
-    Node* n3 = new Node(3);
-    Node* n4 = new Node(4);
-    Node* n5 = new Node(5);
-    Node* n6 = new Node(6);
-    Node* n7 = new Node(7);
-    Node* n8 = new Node(8);
-    head->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-    n5->next = n6;
-    n6->next = n7;
-    n7->next = n8;
-    n8->next = head;
-    Joseph(head,1,1);
+    
+    return 0;
 }
